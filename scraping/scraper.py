0e8c90478d00a27f966402fdf2665dae8c7a6789
Ryan Williams <ryswilli@ucsc.edu>
2026-03-20 19:52:50 -0700

    able to pull all match_ids now from any season, working on getting individual match stats

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -2,9 +2,9 @@ from bs4 import BeautifulSoup
 from datetime import date, timedelta
 from urllib.parse import quote
 from utils.utils import make_request
-from utils.config import SEASON_IDS, SCOREBOARD_URL
+from utils.config import SEASON_IDS, SCOREBOARD_URL, SEASON_DIVISIONS, BOXSCORE_URL
 
-
+# ------------------ MATCH IDS --------------------
 def get_match_ids(url):
     # Array to store all the match ids
     match_ids = []
@@ -31,28 +31,62 @@ def get_match_ids(url):
 def get_all_match_ids(season_id):
     years = SEASON_IDS[season_id]
     start_year, end_year = years.split('-')
-    
-    start_date = date(int(start_year), 2, 1)
-    #end_date = date(int(end_year), 1, 15)
-    end_date = start_date + timedelta(days=10)
+    start_date = date(int(end_year), 1, 1)
+    end_date = date(int(end_year), 4, 30)
 
-    matches = [] 
+    # Iterate through SEASON_DIVISIONS to get divison_id
+    division_id = 0
+    for div_id, div_years in SEASON_DIVISIONS.items():
+        if div_years == years:
+            division_id = div_id
 
     current = start_date
-    while current <= end_date:
-        date_string = quote(current.strftime('%m/%d/%Y'))
-        url = SCOREBOARD_URL.format(season_id=season_id, date=date_string)
-        matches.append(get_match_ids(url))
-        current += timedelta(days=1)
-    
     all_ids = set()
     while current <= end_date:
+        date_string = current.strftime('%m%%2f%d%%2f%Y')
+        print(date_string)
+        url = SCOREBOARD_URL.format(division_id=division_id, date=date_string)
         result = get_match_ids(url)
         if result:
             all_ids.update(result)
         current += timedelta(days=1)
     return all_ids
 
+# ------------------ GAME STATS --------------------
+def get_game_stats(match_id):
+    game_stats = {
+        'team1_name': '', 'team1_id': '', 'team1_sets': '',
+        'team2_name': '', 'team2_id': '', 'team2_sets': '',
+        'winner_id': '',
+        'date': '', 'location': '',
+        'team1_stats': {'kills': 0, 'errors': 0, 'attacks': 0, 'hitting_pct': 0.0, 'digs': 0, 'blocks': 0.0, 'aces': 0, 'service_errors': 0},
+        'team2_stats': {'kills': 0, 'errors': 0, 'attacks': 0, 'hitting_pct': 0.0, 'digs': 0, 'blocks': 0.0, 'aces': 0, 'service_errors': 0},
+    }
+
+    url = BOXSCORE_URL.format(match_id=match_id)
+    response = make_request(url)
+
+    if response is None:
+        return
+    
+    soup = BeautifulSoup(response, 'html.parser')
+
+    # Find the team names and IDs, then add them to the game stats dictionary 
+    team_dict = {}
+    team_tags = soup.find_all('a', class_='skipMask')
+    for tag in team_tags:
+        href_val = tag.get('href')
+        if href_val and href_val.startswith('/teams/'):
+            team_id = href_val.split('/teams/')[1]
+            if team_id not in team_dict.keys() and tag.text.strip():
+                team_dict[team_id] = tag.text.strip()
+    teams = list(team_dict.items())
+    print(teams)
+    game_stats['team1_id'], game_stats['team1_name'] = teams[0]
+    game_stats['team2_id'], game_stats['team2_name'] = teams[1]
+
+    return game_stats
+    
+
 if __name__ == "__main__":
-    arr = get_all_match_ids(16760)
-    print(arr)
+    print(get_game_stats(6081819))
--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,7 @@
 # Base URL for stats.ncaa.org
 BASE_URL = 'https://stats.ncaa.org/'
-SCOREBOARD_URL = 'https://stats.ncaa.org/season_divisions/{division_id}/livestream_scoreboards?game_date={date}'
+SCOREBOARD_URL = 'https://stats.ncaa.org/season_divisions/{division_id}/livestream_scoreboards?utf8=%E2%9C%93&season_division_id=&game_date={date}&conference_id=0&tournament_id=&commit=Submit'
+BOXSCORE_URL = 'https://stats.ncaa.org/contests/{match_id}/box_score'
 # Database path
 DB_PATH = 'database/volleyball.db'
 # Delay between web requests
@@ -25,4 +26,25 @@ SEASON_IDS = {
     10540: "2010-2011",
     10481: "2009-2010",
     11000: "2008-2009",
+}
+# Dict Mapping season_divisions for link creation for db
+SEASON_DIVISIONS = {
+    18745: "2025-2026",
+    18464: "2024-2025",
+    18281: "2023-2024",
+    18121: "2022-2023",
+    17821: "2021-2022",
+    17485: "2020-2021",
+    17021: "2019-2020",
+    16761: "2018-2019",
+    13892: "2017-2018",
+    13220: "2016-2017",
+    12860: "2015-2016",
+    12540: "2014-2015",
+    11780: "2013-2014",
+    10960: "2012-2013",
+    13522: "2011-2012",
+    13427: "2010-2011",
+    13512: "2009-2010",
+    13431: "2008-2009",
 }
